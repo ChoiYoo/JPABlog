129a93531752283fb767dc025eab6b009fa52f9d
yoojung.choi <green980611@naver.com>
2023-04-01 02:31:54 +0900

    initial commit

--- src/main/java/com/example/jpablog/JpaBlogApplication.java
+++ src/main/java/com/example/jpablog/JpaBlogApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JpaBlogApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(JpaBlogApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(JpaBlogApplication.class, args);
+	}
 
 }
--- src/test/java/com/example/jpablog/JpaBlogApplicationTests.java
+++ src/test/java/com/example/jpablog/JpaBlogApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class JpaBlogApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
