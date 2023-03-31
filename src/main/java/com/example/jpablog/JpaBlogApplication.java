3acbbd58f9c87092da8610318687062ecacceae3
yoojung.choi <green980611@naver.com>
2023-04-01 02:05:26 +0900

    Initial commit

---
+++ src/main/java/com/example/jpablog/JpaBlogApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpablog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaBlogApplication.class, args);
+    }
+
+}
