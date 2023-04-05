a92d5367cd146498d4e3a2f771cac31f083d25f0
yoojung.choi <green980611@naver.com>
2023-04-05 23:11:12 +0900

    feat: 게시글 수정

---
+++ src/main/java/com/example/jpablog/notice/model/NoticeInput.java
@@ -0,0 +1,18 @@
+package com.example.jpablog.notice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class NoticeInput {
+
+    private String title;
+    private String contents;
+}
