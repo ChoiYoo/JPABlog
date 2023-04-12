d3bdfb6c8ccd3899465d138864d5934778c177af
yoojung.choi <green980611@naver.com>
2023-04-12 23:45:46 +0900

    주석삭제

--- src/main/java/com/example/jpablog/notice/controller/ApiNoticeController.java
+++ src/main/java/com/example/jpablog/notice/controller/ApiNoticeController.java
@@ -20,48 +20,6 @@ public class ApiNoticeController {
 
     private final NoticeRepository noticeRepository;
 
-//    @GetMapping("/api/notice")
-//    public String noticeString() {
-//
-//        return "공지사항입니다.";
-//    }
-
-//    @GetMapping("/api/notice")
-//    public NoticeModel notice() {
-//
-//        LocalDateTime regDate = LocalDateTime.of(2021, 2, 8, 0, 0);
-//
-//        NoticeModel notice = new NoticeModel();
-//        notice.setId(1);
-//        notice.setTitle("공지사항입니다.");
-//        notice.setContents("공지사항 내용입니다.");
-//        notice.setRegDate(regDate);
-//
-//        return notice;
-//    }
-
-//    @GetMapping("/api/notice")
-//    public List<NoticeModel> notice() {
-//
-//        List<NoticeModel> noticeList = new ArrayList<>();
-//
-//        noticeList.add(NoticeModel.builder()
-//                .id(1)
-//                .title("공지사항입니다.")
-//                .contents("공지사항내용입니다.")
-//                .regDate(LocalDateTime.now())
-//                .build());
-//
-//        noticeList.add(NoticeModel.builder()
-//                .id(2)
-//                .title("두번째 공지사항입니다.")
-//                .contents("두번째 공지사항내용입니다.")
-//                .regDate(LocalDateTime.now())
-//                .build());
-//
-//        return noticeList;
-//    }
-
     @GetMapping("/api/notice")
     public List<NoticeModel> notice() {
 
