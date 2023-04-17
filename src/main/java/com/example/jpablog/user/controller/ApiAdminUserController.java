4f83501d0d743f16fbf6fcbe9cb8449781fe6046
yoojung.choi <green980611@naver.com>
2023-04-17 18:30:16 +0900

    feat: 사용자 관리 55

--- src/main/java/com/example/jpablog/user/controller/ApiAdminUserController.java
+++ src/main/java/com/example/jpablog/user/controller/ApiAdminUserController.java
@@ -124,4 +124,24 @@ public class ApiAdminUserController {
 
         return ResponseEntity.ok().body(ResponseMessage.success());
     }
+
+    @PatchMapping("/api/admin/user/{id}/unlock")
+    public ResponseEntity<?> MemberUnLock(@PathVariable Long id) {
+
+        Optional<Member> optionalMember = memberRepository.findById(id);
+
+        if(!optionalMember.isPresent()) {
+            return new ResponseEntity<>(ResponseMessage.fail("사용자 정보가 없습니다."), HttpStatus.BAD_REQUEST);
+        }
+
+        Member member = optionalMember.get();
+
+        if(!member.isLockYn()) {
+            return new ResponseEntity<>(ResponseMessage.fail("이미 접속제한이 해제된 사용자입니다."), HttpStatus.BAD_REQUEST);
+        }
+        member.setLockYn(false);
+        memberRepository.save(member);
+
+        return ResponseEntity.ok().body(ResponseMessage.success());
+    }
 }
