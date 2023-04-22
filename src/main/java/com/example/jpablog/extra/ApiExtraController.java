42f322b680b86e685a345ee23e65b9058c6adb83
yoojung.choi <green980611@naver.com>
2023-04-22 23:34:26 +0900

    feat: 공공API 활용 89

--- src/main/java/com/example/jpablog/extra/ApiExtraController.java
+++ src/main/java/com/example/jpablog/extra/ApiExtraController.java
@@ -7,6 +7,7 @@ import com.example.jpablog.board.model.*;
 import com.example.jpablog.board.service.BoardService;
 import com.example.jpablog.common.exception.BizException;
 import com.example.jpablog.common.model.ResponseResult;
+import com.example.jpablog.extra.model.AirInput;
 import com.example.jpablog.extra.model.OpenApiResult;
 import com.example.jpablog.extra.model.PharmacySearch;
 import com.example.jpablog.notice.model.ResponseError;
@@ -157,6 +158,34 @@ public class ApiExtraController {
 
         return ResponseResult.success(jsonResult);
     }
+
+    /**
+     * 89. 미세먼지 정보 조회(공공 API)를 통해서 내용을 내리는 API를 작성해 보세요.
+     */
+    @GetMapping("/api/extra/air")
+    public String air(@RequestBody AirInput airInput){
+
+        String apiKey = "qk5UkstORXM6FGpBNmuWw99KXR9X0l1O6UEe0Vp80P19Cdo7xOnsSHDEwDfGna1HoTWoTuejB%2BfSRH2JZmC32A%3D%3D";
+        String url = "https://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty?serviceKey=%s&returnType=json&numOfRows=100&pageNo=1&sidoName=%s";
+
+        String apiResult = "";
+
+        try {
+            URI uri = new URI(String.format(url, apiKey, URLEncoder.encode(airInput.getSearchSido(), "UTF-8")));
+
+            RestTemplate restTemplate = new RestTemplate();
+            HttpHeaders headers = new HttpHeaders();
+            headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+
+            String result = restTemplate.getForObject(uri, String.class);
+            apiResult = result;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return apiResult;
+    }
+
+
 
 
 }
---
+++ src/main/java/com/example/jpablog/extra/model/AirInput.java
@@ -0,0 +1,17 @@
+package com.example.jpablog.extra.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @Builder
+@AllArgsConstructor @NoArgsConstructor
+public class AirInput {
+
+    private String sido;
+
+    public String getSearchSido(){
+        return sido != null ? sido :"";
+    }
+}
