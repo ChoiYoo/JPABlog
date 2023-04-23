917aacf9486c888bfadd50aaf41c0a7cf794d1cd
yoojung.choi <green980611@naver.com>
2023-04-23 21:33:07 +0900

    feat: 카카오 API 활용 90

--- src/main/java/com/example/jpablog/extra/ApiExtraKakaoController.java
+++ src/main/java/com/example/jpablog/extra/ApiExtraKakaoController.java
@@ -1,13 +1,10 @@
 package com.example.jpablog.extra;
 
 import com.example.jpablog.common.model.ResponseResult;
-import com.example.jpablog.extra.model.AirInput;
 import com.example.jpablog.extra.model.KakaoTranslateInput;
-import com.example.jpablog.extra.model.OpenApiResult;
-import com.example.jpablog.extra.model.PharmacySearch;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -18,13 +15,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.net.URI;
-import java.net.URLEncoder;
-import java.util.Collections;
-
 @RequiredArgsConstructor
 @RestController
 public class ApiExtraKakaoController {
+
+    @Value("${KAKAO-API-KEY}")
+    private String kakao_api_key;
+
+    @Value("${KAKAO-URL}")
+    private String kakao_url;
 
     /**
      * 90. KAKAO OPEN API를 활용한 게시글 번역서비스를 구현하는 API를 작성해 보세요.
@@ -33,7 +32,26 @@ public class ApiExtraKakaoController {
     @GetMapping("/api/extra/kakao/translate")
     public ResponseEntity<?> translate(@RequestBody KakaoTranslateInput kakaoTranslateInput){
 
-        return ResponseResult.success(kakaoTranslateInput);
+        String restApiKey = kakao_api_key;
+        String url = kakao_url;
+
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        MultiValueMap<String, String> parameters = new LinkedMultiValueMap<>();
+        parameters.add("src_lang", "kr");
+        parameters.add("target_lang", "en");
+        parameters.add("query", kakaoTranslateInput.getText());
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        headers.add("Authorization", "KakaoAK " + restApiKey);
+
+        HttpEntity formEntity = new HttpEntity<>(parameters, headers);
+        ResponseEntity<String> responseEntity = restTemplate.postForEntity(url, formEntity, String.class);
+
+
+        return ResponseResult.success(responseEntity.getBody());
     }
 
 
